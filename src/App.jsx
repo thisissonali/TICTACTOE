aaa57421ff72574fce10ed0660ed0c852b463026
Sonali Rana <ranasonali987@gmail.com>
2023-11-16 14:31:27 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   const [count, setCount] = useState(1);
   const [chance, setChance] = useState('');
   const [isSelected, setIsSelected] = useState(false);
+  
+  const EMPTY = 0;
+  const ZERO = 1;
+  const CROSS = 2;
+
+  const [grid, setGrid] = useState(Array(9).fill(EMPTY));
 
   useEffect(() => {
     if (count % 2 !== 0) {
@@ -16,106 +22,30 @@ function App() {
     }
     console.log(count);
   }, [count]);
-  
+  console.log(grid.length);
   return (
     <div className="box-cont">
       <div className="container">
         <div className="row">
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            1
-          </button>
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            2
-          </button>
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            3
-          </button>
+          {
+            grid.map((cell, index) => {
+              return (
+                <button
+                  onClick={() => {
+                    setCount((prevCount) => prevCount + 1);
+                    setIsSelected(true);
+                    console.log("Button Clicked. New Count:", count + 1);
+                  }}
+                  className={`cell-cont ${isSelected ? chance : ""}`}
+                >
+                  {index+1}
+                </button>
+              )
+            
+            })
+          }
         </div>
-        <div className="row">
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            4
-          </button>
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            5
-          </button>
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            6
-          </button>
-        </div>
-        <div className="row">
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            7
-          </button>
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            8
-          </button>
-          <button
-            onClick={() => {
-              setCount((prevCount) => prevCount + 1);
-              setIsSelected(true);
-              console.log("Button Clicked. New Count:", count + 1);
-            }}
-            className={`cell-cont ${isSelected ? chance : ""}`}
-          >
-            9
-          </button>
-        </div>
+        
       </div>
     </div>
   );
